3693d7e3c8f0e9d8644f75b59c419ef714d8b50d
Douglas Ferreira <46136914+DouglasF3rreira@users.noreply.github.com>
2018-12-25 17:16:38 -0200

    New feature: add multiple IP's
    If the user choose de option to create a statis IP, now its possible to add multiples IP's that are hard coded in the file.

--- Config_NIC.bat
+++ Config_NIC.bat
@@ -1,21 +1,22 @@
 @echo off
 
-Set StrNicName="Ethernet"
+Set StrNicName="VirtualBox"
+Set NetworkAddr=192.168.1
+Set NetworkMask=255.255.255.0
+Set IPsArrayZSL=(43,37,36,42,34,40,35,38,41,44,19,30,21,24,6,7,9,11,16,61,71,10,12,17,18,31,32,14,23,26,22,28,25,29)
+Set IPsArray=%IPsArrayZSL%
 
 echo Placa de rede %StrNicName%
-echo 1 - Configurar um IP estatico
-echo 2 - Ativar DHCP
+echo 1 - Configurar o array de IPs estaticos
+echo 2 - Ativar DHCP (resetar IPs estaticos)
 echo ---------------------------------------------
 Set /P UserChoice=Escolha 1 ou 2 baseado no menu acima:
 
 If %UserChoice%==1 GoTo ConfigStaticIp
 If %UserChoice%==2 GoTo ConfigDhcp
-exit
 
 :ConfigStaticIp
-Set /P IppAddr=Digite o endereço IP:
-Set /P NetworkMask=Digite a mascara de rede: 
-netsh interface ipv4 set address name=%StrNicName% static %IppAddr% %NetworkMask%
+For %%j in %IPsArray% Do netsh interface ipv4 add address %StrNicName% %NetworkAddr%.%%j %NetworkMask%
 Call :EndOfScript
 
 :ConfigDhcp
@@ -23,6 +24,7 @@ netsh interface ipv4 set address name=%StrNicName% dhcp
 Call :EndOfScript
 
 :EndOfScript
+cls
 echo Fim!
 pause>nul
 exit
